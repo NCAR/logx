4f7ab6859489bb28fdd8bbd21e326ace83cb8995
granger <granger@da89c675-a4d7-0310-8a15-9e7546503515>
2002-10-31 07:16:49 +0000

    convert Entity to all dynamic allocation using constructor methods and hiding constructors; insert entities directly rather than copying first; adapt EntityPtrT to that scheme by adding yield() and a special addMember() which accepts an EntityPtr; add XML, including integrating stream error handler with log4cpp; now all Dimensions are handled as pointers rather than value objects; start moving xml code into DataSpace class, including getClassPrototype()

---
+++ Logging.h
@@ -0,0 +1,19 @@
+// For internal implementation only and not meant to be included by
+// any public interface files.
+
+#ifndef _Logging_H
+#define _Logging_H
+
+#include <log4cpp/Category.hh>
+
+#define LOGGING(name) \
+namespace { \
+log4cpp::Category &CatLog = log4cpp::Category::getInstance(name); \
+log4cpp::CategoryStream dlog = CatLog.debugStream(); \
+log4cpp::CategoryStream elog = CatLog.errorStream(); \
+log4cpp::CategoryStream::Separator endlog = log4cpp::CategoryStream::ENDLINE;\
+}
+
+
+#endif // _Logging_H
+
