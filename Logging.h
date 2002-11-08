3ba74297f2bc7e46ddfeaeb20e421a8f3770f2cb
granger <granger@da89c675-a4d7-0310-8a15-9e7546503515>
2002-11-08 01:44:16 +0000

    change logging to DLOG and ELOG macros; add lots of elements to dtd and examples to dimensions.xml; add recursive, relative datapaths

--- Logging.h
+++ Logging.h
@@ -9,11 +9,11 @@
 #define LOGGING(name) \
 namespace { \
 log4cpp::Category &CatLog = log4cpp::Category::getInstance(name); \
-log4cpp::CategoryStream dlog = CatLog.debugStream(); \
-log4cpp::CategoryStream elog = CatLog.errorStream(); \
 log4cpp::CategoryStream::Separator endlog = log4cpp::CategoryStream::ENDLINE;\
 }
 
+#define DLOG CatLog.debugStream()
+#define ELOG CatLog.errorStream()
 
 #endif // _Logging_H
 
