0ab754b4816f9553543eb60b97fbb6841ad8104a
granger <granger@da89c675-a4d7-0310-8a15-9e7546503515>
2003-02-21 09:35:15 +0000

    add some documentation, especially for the namespace

--- Logging.h
+++ Logging.h
@@ -5,6 +5,11 @@
 
 #include <log4cpp/Category.hh>
 
+/**
+ * Useful extensions to the log4cpp library, including a basic log layout 
+ * class and some command-line argument handling for setting log levels
+ * of known categories.
+ **/
 namespace logx
 {
   /**
@@ -17,12 +22,23 @@ namespace logx
   /**
    * Parse and remove logging options in the command-line argument list.
    * This also sets up a default, basic logging layout and adds it as the
-   * appender for the root category.
+   * appender for the root category.  The categories can be listed with 
+   * the -categories option, but only the categories created statically with
+   * file scope (ie, before main() is entered) will be known at this point.
+   * However, that includes all categories created with the LOGGING macro.
+   *
+   * @see LOGGING
+   * @see LogUsage()
    **/
   void
   ParseLogArgs (int& argc, char* argv[]);
 }
 
+/**
+ * Create a log category with the given @p name with static file scope.
+ * Logging streams for this category can be created with the DLOG, ELOG,
+ * and ILOG macros.
+ **/
 #define LOGGING(name) \
 namespace { \
 log4cpp::Category &CatLog = log4cpp::Category::getInstance(name); \
