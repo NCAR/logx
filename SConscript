1beab877c136fa7827085f4a9153faf917c6fafa
burghart <burghart@da89c675-a4d7-0310-8a15-9e7546503515>
2010-03-19 15:41:41 +0000

    o added tool_logx.py so that the logx tool will be built when required regardless of   where it lives in the source tree
    o SConscript now just "uses" the logx tool (effectively loading tool_logx.py)

--- SConscript
+++ SConscript
@@ -1,23 +1,2 @@
-# -*- python -*-
-
-env = Environment(tools = Split('default doxygen log4cpp'))
-
-sources = Split("""
- Logging.cc LogLayout.cc LogAppender.cc system_error.cc
-""")
-headers = Split("""
- CaptureStream.h EventSource.h Logging.h Checks.h
- system_error.h
-""")
-
-objects = env.SharedObject(sources)
-liblogx = env.Library('logx', objects)
-Default(liblogx)
-
-env.InstallLibrary(liblogx)
-env.InstallHeaders('logx', headers)
-
-env['DOXYFILE_DICT'].update({ "PROJECT_NAME" : "logx library" })
-env.Apidocs(sources + headers + ["private/LogLayout.h"])
-
-SConscript("tests/SConscript")
+# Just cause the logx tool to be loaded.  See tool_logx.py for the meat.
+env = Environment(tools = ['logx'])
---
+++ tool_logx.py
@@ -0,0 +1,33 @@
+# -*- python -*-
+
+tools = ['doxygen', 'log4cpp']
+env = Environment(tools = ['default'] + tools)
+
+logxDir = Dir('.').abspath
+
+def logx(env):
+    env.Append(LIBS=[env.GetGlobalTarget('liblogx'),])
+    env.AppendUnique(CPPPATH = logxDir)
+    env.Require(tools)
+
+Export('logx')
+
+sources = Split("""
+ Logging.cc LogLayout.cc LogAppender.cc system_error.cc
+""")
+headers = Split("""
+ CaptureStream.h EventSource.h Logging.h Checks.h
+ system_error.h
+""")
+
+objects = env.SharedObject(sources)
+lib = env.Library('logx', objects)
+Default(lib)
+
+env.InstallLibrary(lib)
+env.InstallHeaders('logx', headers)
+
+env['DOXYFILE_DICT'].update({ "PROJECT_NAME" : "logx library" })
+env.Apidocs(sources + headers + ["private/LogLayout.h"])
+
+SConscript("tests/SConscript")
